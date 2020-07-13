44b5ad25a987dfeda833fe4557ab5f7b1265b4df
nscuro <nscuro@protonmail.com>
2020-07-13 22:23:48 +0200

    Fix pipeline script

--- examples/pipeline.sh
+++ examples/pipeline.sh
@@ -7,10 +7,7 @@ echo "[+] Performing FDNS enumeration"
 fdnssearch --amass-config amass.ini -t a -t aaaa -c 25 --no-ansi | tee -a domains.txt
 
 echo "[+] Deduplicating results..."
-sort --unique -o domains_dedupe.txt domains.txt
-
-echo "[+] Cleaning up..."
-rm results.txt && mv domains_dedupe.txt domains.txt
+sort --unique -o domains.txt domains.txt
 
 echo "[+] Resolving domains..."
 massdns --resolvers $MASSDNS_PATH/lists/resolvers.txt -o J -w resolved_domains.json domains.txt
