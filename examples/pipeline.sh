14f0440ccd9e22aad71ba91d01a067318c4058d0
Niklas <nscuro@protonmail.com>
2020-07-14 10:38:41 +0200

    Cosmetics

--- examples/pipeline.sh
+++ examples/pipeline.sh
@@ -3,11 +3,14 @@
 echo "[+] Performing passive amass enumeration..."
 amass enum -passive -config amass.ini -nolocaldb -o domains.txt
 
-echo "[+] Performing FDNS enumeration"
+echo "[+] Performing FDNS enumeration..."
 fdnssearch --amass-config amass.ini -t a -t aaaa -c 25 --no-ansi | tee -a domains.txt
 
 echo "[+] Deduplicating results..."
 sort --unique -o domains.txt domains.txt
 
 echo "[+] Resolving domains..."
-massdns --resolvers $MASSDNS_PATH/lists/resolvers.txt -o J -w resolved_domains.json domains.txt
+massdns --resolvers $MASSDNS_PATH/lists/resolvers.txt -o J -w domains_resolved.json domains.txt
+
+# Probe for HTTP/S servers with httprobe
+# ...
